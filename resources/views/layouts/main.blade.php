ad8d8eb1ad64a509749a07074b9381953bed0cb0
iliabuianov <ilia.buianov@movesion.com>
2020-05-12 16:33:30 +0300

    styles and general stuff

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="ru">
+    <head>
+        <meta charset="utf-8">
+        <title>С днём рождения!</title>
+
+    </head>
+    <body>
+        @yield('content')
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/{path?}', function () {
-    return view('dist.index');
+    return view('welcome');
 });
